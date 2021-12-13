b26c816d715c73206a1d563a1d4a546a6dd2bfd3
kevinchao4444 <kevinchao4444@gmail.com>
2021-12-12 23:20:27 -0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -26,8 +26,8 @@ for i in range(strlen):
         gamma += '1'
         epsilon += '0'
 
-print(gamma)
-print(epsilon)
+#print(gamma)
+#print(epsilon)
 
 def binaryToDecimal(binary):
      
@@ -45,3 +45,33 @@ gamma = binaryToDecimal(int(gamma))
 epsilon = binaryToDecimal(int(epsilon))
 
 print(gamma*epsilon)
+
+#----------------------PART 2------------------------------------
+
+array2 = array
+temp = []
+temp2 = []
+strlen2 = len(array2[0])
+
+while (len(array2) > 1):
+    for i in range(strlen2):
+        count1 = 0
+        count0 = 0
+
+        for a in range(len(array2)):
+            if (array2[a][i] == '0'):
+                count0 += 1
+                temp.append(array2[a])
+            else:
+                count1 += 1 
+                temp2.append(array2[a])
+
+        if (count0 > count1):
+            array2 = temp
+            print(array2)
+        else:
+            array2 = temp2
+            print(array2)
+    
+    
+print(array2)
